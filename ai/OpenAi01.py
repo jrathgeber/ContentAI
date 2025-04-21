8b557ffdd56716ed32b4e65a38c1268449f5de26
jrathgeber <jrathgeber@yahoo.com>
2025-04-21 06:04:14 -0400

    tweak some paths

--- ai/OpenAi01.py
+++ ai/OpenAi01.py
@@ -8,17 +8,13 @@ config.read('C:\\etc\\properties.ini')
 
 def write_article(key_words, further_info):
 
-    # Get Reference to Properties
-    config = configparser.ConfigParser()
-    config.read('C:\\etc\\properties.ini')
-
     client = OpenAI(
         # This is the default and can be omitted
         api_key=config['openai']['api_key'],
     )
 
     GPT_MODEL = 'gpt-4o-mini'
-    O1_MODEL = 'o1-mini'
+    #GPT_MODEL = 'o1-mini'
 
     prepend = "Answer should be embedded in html tags and that's it. Nothing else. No quotes"
     instructions = "Write a short blog post titled :  "
@@ -26,7 +22,7 @@ def write_article(key_words, further_info):
     further_info = further_info
 
     prompt = prepend + " " + instructions + " " + keywords + " " + further_info
-    response = client.chat.completions.create(model=O1_MODEL,messages=[{"role":"user","content": prompt}])
+    response = client.chat.completions.create(model=GPT_MODEL,messages=[{"role":"user","content": prompt}])
 
     print(response.choices[0].message.content)
 
--- mediun/get_article.py
+++ mediun/get_article.py
@@ -4,18 +4,13 @@ from medium_api import Medium
 
 # Get Reference to Properties
 config = configparser.ConfigParser()
-config.read('C:\etc\properties.ini')
+config.read('C:\\etc\\properties.ini')
 
-# Numerai credentials for submission
+# Get the medium token
 api_key = config['medium']['token']
 
 # Create a `Medium` Object
 medium = Medium(api_key)
-
-def save_html_to_file(html_content, file_path):
-    with open(file_path, 'w', encoding='utf-8') as file:
-        file.write(html_content)
-    print(f"HTML content saved to: {file_path}")
 
 # Get the "Article" object and print markdown
 article = medium.article(article_id="c4b041243c47")
@@ -23,8 +18,9 @@ article = medium.article(article_id="c4b041243c47")
 
 print(article.html)
 
-html_content =  article.html
-file_path = "C:\\dev\\godaddy\\vcard\\blogpost\Articles\\article_021_will_ai_replace_all_programmers.html"
+html_content = article.html
+file_path = "C:\\dev\\godaddy\\vcard\\blogpost\\Articles\\article_021_will_ai_replace_all_programmers.html"
 
-save_html_to_file(html_content, file_path)
-
+with open(file_path, 'w', encoding='utf-8') as file:
+    file.write(html_content)
+print(f"HTML content saved to: {file_path}")
