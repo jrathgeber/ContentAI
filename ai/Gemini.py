cdb4b3ffe6a3b8ea657ba95307971b831e268f39
jrathgeber <jrathgeber@yahoo.com>
2025-04-25 06:49:28 -0400

    gemini

--- ai/Gemini.py
+++ ai/Gemini.py
@@ -1,6 +1,21 @@
 import google.generativeai as genai
 import configparser
 
+# Get Reference to Properties
+config = configparser.ConfigParser()
+config.read('C:\\etc\\properties.ini')
+
+api_key = config['gemini']['api_key']
+
+
+def list_models():
+
+    genai.configure(api_key=api_key)
+
+    for m in genai.list_models():
+        if "generateContent" in m.supported_generation_methods:
+            print(m.name)
+
 
 def generate_article(model_name, prompt):
 
@@ -15,12 +30,6 @@ def generate_article(model_name, prompt):
         The generated article text, or None if an error occurs.
     """
 
-    # Get Reference to Properties
-    config = configparser.ConfigParser()
-    config.read('C:\\etc\\properties.ini')
-
-    api_key = config['gemini']['api_key']
-
     try:
         genai.configure(api_key=api_key)
         model = genai.GenerativeModel(model_name)
@@ -33,8 +42,12 @@ def generate_article(model_name, prompt):
 
 if __name__ == "__main__":
 
+    print("Hello Gemini")
+
+    list_models()
+
     model_name = "gemini-2.0-flash"
-    prompt = "Write a short article about the benefits of walking for 30 minutes a day."
+    prompt = "Tell me what is going on with this web site : https://agentverse.ai/?sort=relevancy&page=1&recommended=true"
 
     article = generate_article(model_name, prompt)
 
