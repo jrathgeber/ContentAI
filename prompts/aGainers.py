708115fa55c0ddabf58c91e6524d1a503e83ab11
jrathgeber <jrathgeber@yahoo.com>
2025-05-16 20:03:48 -0400

    prompt

--- prompts/aGainers.py
+++ prompts/aGainers.py
@@ -5,11 +5,11 @@ def get_prompt(ticker_list):
     
 Provide super concise info about the following stocks. 
 
-If a stock can't be found just ignore it and say nothing. Don't say not found.  
+If a stock can't be found just ignore it and say nothing. Don't say not found.  Dont say (No in found). Keep it concise. 
 
 Don't provide citation numbers. Don't use asterisk or dashes.
 
-Don't introduce the list just get to the stocks. 
+Don't introduce the list and provide details on stocks you can find. 
 
 Say thanks for reading at the end. Something like "Follow me for stock and financial tweets". Change the ending every time. 
         
