8c73ee217444d25b70ee774025b262c488bc4548
jrathgeber <jrathgeber@yahoo.com>
2025-06-01 10:19:51 -0400

    sunday ct

--- twitter/post_img_openai.py
+++ twitter/post_img_openai.py
@@ -1,18 +1,22 @@
 import ai.OpenAIImage as ai
 
+import configparser
+config = configparser.ConfigParser()
+config.read('C:\\etc\\properties.ini')
+
 
 def new_image(key_words):
 
+    # set some vars
     key_words = key_words.lstrip()
-
     slug = key_words.replace(" ", "_")
-
     file_name = slug + '.jpg'
 
+    # new API returns the bytes
     image_bytes = ai.get_openai_image_bytes(key_words)
 
-    file_image_path = "C:\\dep\\ContentAI\\zTemp\\images\\" + file_name
-    file_image_path = "C:\\Users\\jrath\\PycharmProjects\\ContentAI\\zTemp\\images\\" + file_name
+    temp_path = str(config['blog']['blog_temp'])
+    file_image_path = temp_path + "images\\" + file_name
 
     # Save the image to a file
     with open(file_image_path, "wb") as f:
