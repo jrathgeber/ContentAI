cbd863bf33d0e136af8299816513e33350ba075c
Jason Rathgeber <jrathgeber@yahoo.com>
2025-06-02 22:05:31 -0400

    stuff

--- Batch_Scheduler.py
+++ Batch_Scheduler.py
@@ -1,11 +1,11 @@
 import time
 import datetime
 import schedule
-
 import notion.download_data as nt
 import emailxx.yahoo_quick_email as yahoo
-from twitter.tweet import tweetSomething
-
+import twitter.tweet
+import twitter.tweet_image_v2
+import twitter.post_img_openai
 
 def good_morning():
 
@@ -16,7 +16,7 @@ def good_morning():
     print("Tweeting :: " + tweet)
     yahoo.send_quick_message('ContentAI is Live!', tweet)
 
-    tweetSomething("GM X. " + tweet)
+    tweet.tweetSomething("GM X hustlers. Today remember :  " + tweet)
 
 def midday_motivation():
 
@@ -33,15 +33,21 @@ def good_night():
     current_time = datetime.datetime.now().strftime("%H:%M:%S")
     print(f"Current time: {current_time}")
 
-    tweet = nt.get_great_content_from_notion("Manifest")
+    fact = nt.get_great_content_from_notion("Astronomy")
+    img = twitter.post_img_openai.new_image(fact)
+
+    tweet = "Good night X. Something to think about as you drift off : " + fact
+
+    twitter.tweet_image_v2.post_image_tweet(img, tweet.lstrip())
+
     print("Tweeting :: " + tweet)
-    yahoo.send_quick_message('ContentAI is Live!', tweet)
+    yahoo.send_quick_message('ContentAI is going to bed!', tweet)
 
 
 # Schedule the stuff
 schedule.every().day.at("08:00").do(good_morning)
 schedule.every().day.at("12:00").do(midday_motivation)
-schedule.every().day.at("20:00").do(good_night)
+schedule.every().day.at("22:00").do(good_night)
 
 print("Scheduler started. Waiting for scheduled times...")
 print("Scheduled times: 8:00 AM, 12:00 PM, and 8:00 PM")
